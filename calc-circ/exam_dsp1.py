ce6052c8205edaeeffe7a02cdcf2f71eb3b1fa17
Milton Sarria-Paja <miltonsarria@gmail.com>
2018-03-21 14:45:03 -0500

    exam circ

--- calc-circ/exam_dsp1.py
+++ calc-circ/exam_dsp1.py
@@ -1,16 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-n=np.arange(1000)
-x=np.cos(np.pi*n/8)
-
+#generar la secuencia h
 n=np.arange(40)
 arg=np.pi*(n-20)/10
-
 h=np.sin(arg)/(arg)
 h[20]=1
+
+
+#generar la secuencia x
+n=np.arange(1000)
+x=np.cos(np.pi*n/4)
+
+#generar la secuencia y=x*h
 y=np.convolve(x,h)
 
+#graficar las secuencias
 plt.plot(h)
 plt.figure()
 plt.plot(x)
